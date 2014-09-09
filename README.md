cbbe380246c3225a0fbe98253228cad0d248d715
marsbaiyun <marsbaiyp@gmail.com>
2014-09-09 10:44:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+nblog
+=====
+
+this is N-Blog
---
+++ settings.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Mars on 2014/9/5 0005.
+ */
+module.exports = {
+    cookieecret: 'myblog',
+    db: 'blog',
+    host: 'localhost'
+}
